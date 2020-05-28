35d07409529e1b0e63ac97f66a87a10f4f34e611
saiteja8848 <saiteja8848@gmail.com>
2020-05-28 08:27:39 +0530

    FSJ0420A102

--- dml-pkl.sql
+++ dml-pkl.sql
@@ -2,28 +2,47 @@
 -- PROGRESSION 1:
 
 --1. Insert into city
+INSERT INTO CITY VALUES(3,'INDIA');
 
 --2. Insert into referee
+INSERT INTO REFEREE VALUES(1,'billy');
+
 
 --3. Insert into innings
+INSERT INTO INNINGS VALUES(3,2);
+
 
 --4. Insert into extra_type
+INSERT INTO EXTRA_TYPE VALUES(1,'Y');
+
 
 --5. Insert into skill
+INSERT INTO SKILL VALUES(1,'RAIDER');
 
 --6. Insert into team
+INSERT INTO TEAM VALUES(1,'INDIANS','TOM','3')
+
 
 --7. Insert into player
+INSERT INTO PLAYER VALUES(1,'DHONI','INDIA','1','3');
 
 --8. Insert into venue
+INSERT INTO VENUE VALUES(1,'chinnasway','3');
+
 
 --9. Insert into event
+INSERT INTO EVENT VALUES(1,1,1,1,10,1,60,10,1);
+
 
 --10. Insert into extra_event
+INSERT INTO EXTRA_EVENT VALUES(1,1,1,10,2);
 
 --11. Insert into outcome
+INSERT INTO OUTCOME VALUES(1,'WON',1,10,1);
+
 
 --12. Insert into game
+
 
 --13. Update player table
 
